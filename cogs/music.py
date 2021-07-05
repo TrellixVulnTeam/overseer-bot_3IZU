9486533bb71f0e51cef6a61bc0ee6a29063f2a90
Gedeon Sainrival <gedeonsainrival1@gmail.com>
2021-07-05 11:39:41 -0400

    Added Music join/leave

--- cogs/music.py
+++ cogs/music.py
@@ -10,10 +10,12 @@ class Music(commands.Cog):
     async def join(self, ctx):
         channel = ctx.author.voice.channel
         await channel.connect()
+        await ctx.send(f"Connected to {channel}.")
 
     @commands.command(aliases = ['scram', 'exit'])
     async def leave(self, ctx):
         await ctx.voice_client.disconnect()
+        await ctx.send(f"Exited {channel}.")
 
 def setup(client):
     client.add_cog(Music(client))
